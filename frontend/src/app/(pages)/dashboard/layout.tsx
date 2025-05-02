c8903ca9b37fcfef82b2c6db2e464fce50cb9306
gabriel <ga.lima2205@gmail.com>
2025-05-01 23:19:11 -0300

    init  front

---
+++ frontend/src/app/(pages)/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+
+import "./dasboard.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <div
+
+      >
+        {children}
+      </div>
+  );
+}
---
+++ frontend/src/components/chart-area-interactive.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import * as React from "react"
+
+
+export const description = "An interactive area chart"
+
+export function ChartAreaInteractive() {
+  
+  return (
+    <>
+    
+    </>
+  )
+}
