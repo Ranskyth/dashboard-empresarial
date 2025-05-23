6681e4f190387119d2755a1a81f436ec754a623c
ranskyth <ga.lima2205@gmail.com>
2025-05-22 22:24:42 -0300

    update docker

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -2,7 +2,6 @@ import { FastifyReply, FastifyRequest } from "fastify";
 
 export const verifyJwt = async (req: FastifyRequest, res: FastifyReply) => {
     const { token } = req.cookies
-    console.log(token)
     if (!token) return res.status(401).send({ mensagem: "nenhum token no request" })
 
     try {
